a72db740462b848534d4639af060cd9744329961
AnoukBor <a.m.bor@tilburguniversity.edu>
2022-09-15 11:29:31 +0200

    downlaod data

--- src/data-preparation/data_transformation.R
+++ src/data-preparation/data_transformation.R
@@ -1,4 +1,12 @@
 ### DATA TRANSFORMATION ###
 ## DOWNLOAD DATA ##
 
-listings_amsterdam <- 
+listings_amsterdam 
+
+urls = c('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/visualisations/listings.csv', 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/visualisations/reviews.csv')
+
+for (url in urls) {
+    filename = paste(gsub('[^a-zA-Z]', '', url), '.csv') 
+    filename = gsub('httpdatainsideairbnbcom', '', filename) 
+    download.file(url, destfile = filename) 
+}
