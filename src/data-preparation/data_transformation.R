74e97db05620982a33f42e8703e671025ad843b1
AnoukBor <a.m.bor@tilburguniversity.edu>
2022-09-22 11:32:16 +0200

    filter calendar

--- src/data-preparation/data_transformation.R
+++ src/data-preparation/data_transformation.R
@@ -14,11 +14,19 @@ library('readr')
 library('stringr')
 
 # FILTER VARIABLES NEEDED #
-list_ams_filtered <- c('id', 'host_id', 'neighborhood_overview', 'host_since', 'hosts_location', 'host_neighbourhood', 'neighbourhood_cleansed', 'room_type', 'minimum_nights', 'price')
+list_ams_filtered <- c('id', 'host_id', 'name', 'host_since', 'hosts_location', 'host_neighbourhood', 'neighbourhood_cleansed', 'room_type', 'property_type', 'minimum_nights', 'beds', 'price')
 data_frame_ams <- list_ams[,which(colnames(list_ams)%in%list_ams_filtered)]
 
 reviews_ams_filterd <-c('date', 'reviewer_id', 'reviewer_name', 'comments')
 data_frame_ams_review <- reviews_ams[,which(colnames(reviews_ams)%in%reviews_ams_filterd)]
+
+calendar_ams_filtered <- c('listing_id', 'date','available','price', 'minimum_nights')
+calendar_ams_filtered <- calendar_ams[,which(colnames(calendar_ams)%in%calendar_ams_filtered)]
+
+
+
+
+
 
 # RENAMING VARIABLES # 
 rename(data_frame_ams, #willen we namen veranderen van variabelen? 
