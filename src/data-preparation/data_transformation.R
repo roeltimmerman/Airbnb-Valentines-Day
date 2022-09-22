dc739cde5b56634a215d50714460dafee7ebd42c
Mandana Khabbazi <m.i.khabbazi@tilburguniversity.edu>
2022-09-22 11:35:25 +0200

    little changes

--- src/data-preparation/data_transformation.R
+++ src/data-preparation/data_transformation.R
@@ -15,10 +15,10 @@ library('stringr')
 
 # FILTER VARIABLES NEEDED #
 list_ams_filtered <- c('id', 'host_id', 'name', 'host_since', 'hosts_location', 'host_neighbourhood', 'neighbourhood_cleansed', 'room_type', 'property_type', 'minimum_nights', 'beds', 'price')
-data_frame_ams <- list_ams[,which(colnames(list_ams)%in%list_ams_filtered)]
+list_ams_filtered <- list_ams[,which(colnames(list_ams)%in%list_ams_filtered)]
 
 reviews_ams_filterd <-c('date', 'reviewer_id', 'reviewer_name', 'comments')
-data_frame_ams_review <- reviews_ams[,which(colnames(reviews_ams)%in%reviews_ams_filterd)]
+reviews_ams_filtered <- reviews_ams[,which(colnames(reviews_ams)%in%reviews_ams_filterd)]
 
 calendar_ams_filtered <- c('listing_id', 'date','available','price', 'minimum_nights')
 calendar_ams_filtered <- calendar_ams[,which(colnames(calendar_ams)%in%calendar_ams_filtered)]
