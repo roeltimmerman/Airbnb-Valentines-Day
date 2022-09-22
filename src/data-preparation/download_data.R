c2d879eea7b8a1f3d7f3ffb09920e138e6712e64
AnoukBor <a.m.bor@tilburguniversity.edu>
2022-09-22 11:28:13 +0200

    download calendar data

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -6,7 +6,9 @@ dir.create('../../data')
 files = list(c(url='http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/data/listings.csv.gz',
                fn='listings-amsterdam.csv.gz'),
              c(url='http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/data/reviews.csv.gz',
-               fn='reviews-amsterdam.csv.gz'))
+               fn='reviews-amsterdam.csv.gz'),
+             c(url='http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-09-07/data/calendar.csv.gz',
+               fn='calendar.csv.gz'))
 
 for (item in files) {
     download.file(item['url'], paste0('../../data/', item['fn']))
@@ -14,3 +16,4 @@ for (item in files) {
 
 list_ams <- read.csv(gzfile('../../data/listings-amsterdam.csv.gz')) 
 reviews_ams <- read.csv(gzfile('../../data/reviews-amsterdam.csv.gz'))
+calendar_ams <- read.csv(gzfile('../../data/calendar.csv.gz'))
