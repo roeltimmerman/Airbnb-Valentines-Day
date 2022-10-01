9bae8557e481b2fe501dc14121c6d882e1e3180c
Eva Bos <e.s.f.bos@tilburguniversity.edu>
2022-10-01 13:23:25 +0200

    deleted review files download + removed the store data part --> moved that to import part of ITO for Data Cleaning

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -12,37 +12,16 @@ files = list(c(url='http://data.insideairbnb.com/france/ile-de-france/paris/2021
                fn ='listings-paris.csv.gz'),
              c(url='http://data.insideairbnb.com/france/ile-de-france/paris/2021-12-07/data/calendar.csv.gz',
                fn='calendar-paris.csv.gz'),
-             c(url='http://data.insideairbnb.com/france/ile-de-france/paris/2021-12-07/data/reviews.csv.gz',
-               fn='reviews-paris.csv.gz'),
              c(url ='http://data.insideairbnb.com/italy/lazio/rome/2021-12-08/data/listings.csv.gz',
                fn='listings-rome.csv.gz'),
              c(url='http://data.insideairbnb.com/italy/lazio/rome/2021-12-08/data/calendar.csv.gz',
                fn='calendar-rome.csv.gz'),
-             c(url='http://data.insideairbnb.com/italy/lazio/rome/2021-12-08/data/reviews.csv.gz',
-               fn='reviews-rome.csv.gz'),
              c(url='http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-12-08/data/listings.csv.gz',
                fn='listings-madrid.csv.gz'),
              c(url='http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-12-08/data/calendar.csv.gz',
-               fn='calendar-madrid.csv.gz'),
-             c(url='http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-12-08/data/reviews.csv.gz',
-               fn='reviews-madrid.csv.gz'))
+               fn='calendar-madrid.csv.gz'))
 
 
 for (item in files) {
     download.file(item['url'], paste0('../../data/', item['fn']))
 }
-
-# store data for Paris
-list_par <- read.csv(gzfile('../../data/listings-paris.csv.gz')) 
-calendar_par <- read.csv(gzfile('../../data/calendar-paris.csv.gz'))
-reviews_par <- read.csv(gzfile('../../data/reviews-paris.csv.gz'))
-
-# store data for Rome
-list_rom <- read.csv(gzfile('../../data/listings-rome.csv.gz')) 
-calendar_rom <- read.csv(gzfile('../../data/calendar-rome.csv.gz'))
-reviews_rom <- read.csv(gzfile('../../data/reviews-rome.csv.gz'))
-
-# store data for Madrid
-list_mad <- read.csv(gzfile('../../data/listings-madrid.csv.gz')) 
-calendar_mad <- read.csv(gzfile('../../data/calendar-madrid.csv.gz'))
-reviews_mad <- read.csv(gzfile('../../data/reviews-madrid.csv.gz'))
