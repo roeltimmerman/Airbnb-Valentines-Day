1694cb11bf5c7e43603c11a17351afb3a871cb80
indiwieggers123 <i.wieggers@tilburguniversity.edu>
2022-10-04 15:55:03 +0200

    Added T-test assumptions & finished plot
    Co-Authored-By: AnoukBor <90313183+AnoukBor@users.noreply.github.com>

--- src/data-preparation/Analyse.R
+++ src/data-preparation/Analyse.R
@@ -1,32 +1,41 @@
 ### ANALYSIS ###
+
+# add to other documents
+install.packages('ggpubr')
+library(ggpubr)
+
 ## T.TEST FOR PRICE AND VALENTINESDAY ##
-# Price in total #
+# Assumptions for T-test
+#Outliers-> no outliers in boxplot. 
+#Normality no necessary as there are no outliers and sample size>25 per group   
+    #(https://eje.bioscientifica.com/view/journals/eje/182/2/EJE-19-0922.xml)
+
+# Price in total # --> not significant
 complete_data$price <- as.numeric(as.factor(complete_data$price))
 t_test_price <- t.test(price ~ valentinesday, complete_data)
 t_test_price
-
-library(dplyr)
-group_by(complete_data, complete_data$valentinesday) %>% 
-    summarise(
-        count = n(),
-        mean = mean(complete_data$price, na.rm = TRUE),
-        sd =  sd(complete_data$price, na.rm = TRUE)
-    )
-
-install.packages('ggpubr')
-library(ggpubr)
+# Plot for total
 ggboxplot(complete_data, x="valentinesday", y="price",
           color="valentinesday", palette = c("#00AFBB", "#E7B800"),
           ylab= "Price", xlab="Valentinesday")
 
+hist(complete_data$price, xlab = 'price')
+
 # Price per city #
 lapply(split(complete_data, factor(complete_data$city)), function(x)t.test(data=x, price ~ valentinesday, paired=FALSE)) 
-
+# Plot for different cities
 ggplot(complete_data, aes(x=valentinesday, y=price, fill=city)) + 
     geom_boxplot() +
     facet_wrap(~valentinesday, scale="free")
 
+
+-------------------------
+
+
 ## LOGISTIC REGRESSION FOR BOOKINGS AND VALENTINESDAY ##
+# Assumptions logistic regression
+
+    
 # Bookings in total #
 glm1 <- glm(booked ~ valentinesday, complete_data, family = binomial)
 exp(glm1$coefficients)
