8dfbcdd9b7aa6a273e19045a8f960287e1d9fae5
AnoukBor <a.m.bor@tilburguniversity.edu>
2022-10-06 09:47:53 +0200

    added 1 to name

---
+++ src/analysis/analysis1.R
@@ -0,0 +1,46 @@
+#### Analysis 1 t-test for price and valentinesday 
+## Library 
+library(ggplot2)
+library(tidyverse)
+library(ggpubr)
+
+## Input ##
+complete_data <- read.csv("../../gen/temp/complete_data.csv") 
+
+# Data transformation for analysis 1 
+complete_data$price <- as.numeric(as.factor(complete_data$price)) 
+
+# Descriptive statistics 
+summary(complete_data$price)
+table(complete_data$price)
+hist(complete_data$price, xlab = 'price') 
+
+summary(complete_data$valentinesday)
+table(complete_data$valentinesday)
+
+# Assumptions (outliers)
+ggboxplot(complete_data, x="valentinesday", y="price",
+          color="valentinesday", palette = c("#00AFBB", "#E7B800"),
+          ylab= "Price", xlab="Valentinesday")
+
+# Price in total 
+t_test_price <- t.test(price ~ valentinesday, complete_data) 
+t_test_price 
+
+# Plot for total 
+ggboxplot(complete_data, x="valentinesday", y="price",
+          color="valentinesday", palette = c("#00AFBB", "#E7B800"),
+          ylab= "Price", xlab="Valentinesday")
+
+hist(complete_data$price, xlab = 'price in €') 
+
+# Price per city # 
+lapply(split(complete_data, factor(complete_data$city)), function(x)t.test(data=x, price ~ valentinesday, paired=FALSE))
+
+# Plot for different cities 
+ggplot(complete_data, aes(x=valentinesday, y=price, fill=city)) + 
+    geom_boxplot() +
+    facet_wrap(~valentinesday, scale="free") +
+    theme(axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())
+
