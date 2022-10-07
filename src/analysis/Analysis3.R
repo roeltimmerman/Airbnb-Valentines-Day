4022f94ce3868a29f067d4fb7f04a273bb1b862c
indiwieggers123 <i.wieggers@tilburguniversity.edu>
2022-10-07 15:47:20 +0200

    Removed not-booked listings

--- src/analysis/Analysis3.R
+++ src/analysis/Analysis3.R
@@ -7,21 +7,23 @@ library(ggplot2)
 complete_data <- read.csv("../../gen/data-preparation/temp/complete_data.csv") 
 
 # Data transformation for analysis 3
-complete_data$beds_dummy <- ifelse(complete_data$beds <= 2, 1, 0)
+complete_data_booked <- complete_data %>% 
+    filter(complete_data$booked == 1)
+complete_data_booked$beds_dummy <- ifelse(complete_data$beds <= 2, 1, 0)
 
 # Descriptive statistics
-summary(complete_data$beds_dummy)
-data_beds <- table(complete_data$valentinesday, complete_data$beds_dummy)
+summary(complete_data_booked$beds_dummy)
+data_beds <- table(complete_data_booked$valentinesday, complete_data_booked$beds_dummy)
 data_beds
 ## HIER MOET NOG DE BARPLOT 
 
 # Assumptions (normality)
 set.seed(5000)
-complete_data_sample <- rnorm(5000)
+complete_data_booked_sample <- rnorm(5000)
 shapiro.test(complete_data_sample)
 
 # Logistic regression total 
-glm2 <- glm(beds_dummy ~ valentinesday, data = complete_data)
+glm2 <- glm(beds_dummy ~ valentinesday, data = complete_data_booked)
 glm2
 exp(glm2$coefficients)
 
@@ -31,7 +33,7 @@ glm2_chisqdf <- glm2$df.null-glm2$df.residual
 1-pchisq(glm2_chisq,glm2_chisqdf) 
 
 # Logistic regression per city 
-glm2_per_city <- lapply(split(complete_data, factor(complete_data$city)), function(x)glm(data=x, beds_dummy ~ valentinesday))
+glm2_per_city <- lapply(split(complete_data_booked, factor(complete_data_booked$city)), function(x)glm(data=x, beds_dummy ~ valentinesday))
 glm2_per_city
 
 lapply(glm2_per_city, function(x) 
