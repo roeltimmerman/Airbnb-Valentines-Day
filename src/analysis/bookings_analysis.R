20fa6884e37d54bf281e1779afaa006fbc9f01bd
bixuanguo <bixuanguo1998@hotmail.com>
2022-10-13 02:54:16 +0200

    added stargazer for analysis

--- src/analysis/bookings_analysis.R
+++ src/analysis/bookings_analysis.R
@@ -6,9 +6,13 @@
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
+library(stargazer)
 
 ## INPUT ##
 complete_data <- read.csv("../../gen/data-preparation/temp/complete_data.csv") 
+complete_data_mad <- read.csv("../../gen/data-preparation/temp/complete_data_booked_mad.csv")
+complete_data_par <- read.csv("../../gen/data-preparation/temp/complete_data_booked_par.csv")
+complete_data_rom <- read.csv("../../gen/data-preparation/temp/complete_data_booked_rom.csv")
 
 ## TRANSFORMATION ##
 summary(complete_data$booked)
@@ -44,9 +48,27 @@ glm1_chisqdf <- glm1$df.null-glm1$df.residual
 glm1_per_city <- lapply(split(complete_data, factor(complete_data$city)), function(x)glm(data=x, booked ~ valentinesday))
 lapply(glm1_per_city, function(x) 
 exp(x$coefficients))
+# Deze waardes kloppen niet...
+glm1_m1 <- glm(booked ~ valentinesday, complete_data_mad, family = binomial)
+exp(glm1_m1$coefficients)
+glm1_m2 <- glm(booked ~ valentinesday, complete_data_par, family = binomial)
+exp(glm1_m2$coefficients)
+glm1_m3 <- glm(booked ~ valentinesday, complete_data_rom, family = binomial)
+exp(glm1_m3$coefficients)
 
 ## OUTPUT ##
 pdf(file="../../gen/analysis/output/histogram_booked.pdf")
 dev.off()
 pdf(file="../../gen/analysis/output/histogram_total_bookings.pdf")
 dev.off()
+stargazer(glm1_m1, glm1_m2, glm1_m3, apply.coef=exp, apply.se = exp, type="html", title="Effect of Valentine's Day on number of bookings of Airbnb listings",
+          dep.var.caption = "Number of bookings",
+          dep.var.labels="",
+          model.numbers = FALSE,
+          column.labels = c('Madrid', 'Paris', 'Rome'),
+          covariate.labels="Valentine's Day", out='../../gen/analysis/output/model_bookings_city.html')
+stargazer(glm1, apply.coef=exp, apply.se = exp, type="html", title="Effect of Valentine's Day on number of bookings of Airbnb listings",
+          dep.var.caption = "Number of bookings",
+          dep.var.labels="",
+          column.labels = 'Total',
+          covariate.labels="Valentine's Day", out='../../gen/analysis/output/model_bookings.html')
